2a5231e0e0bcd44dfd63cc19929831c7dabd8c48
sylvain-schmitt <69637905+sylvain-schmitt@users.noreply.github.com>
2020-11-25 17:17:40 +0100

    Add: home page + details annonces page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Repository\AnnoncesRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(AnnoncesRepository $annoncesRepo)
     {
+        $annonces = $annoncesRepo->findBy(['active' => true], ['created_at' => 'desc'], 10);
+
+
+        if (!$annonces) {
+            throw new NotFoundHttpException('Aucunes annonces trouvée');
+        }
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'annonces' => $annonces,
+        ]);
+    }
+
+    /**
+     * @Route("/details/{slug}", name="annonces_details")
+     */
+    public function details($slug, AnnoncesRepository $annoncesRepo)
+    {
+        $annonce = $annoncesRepo->findOneBy(['slug' => $slug]);
+
+        if (!$annonce) {
+            throw new NotFoundHttpException('Pas d\'annonce trouvée');
+        }
+
+
+
+        return $this->render('main/details.html.twig', [
+            'annonce' => $annonce,
         ]);
     }
 }
