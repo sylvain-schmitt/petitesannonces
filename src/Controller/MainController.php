c1f7f3f2579b3e48adf7e03a218623e83d7815fe
sylvain-schmitt <69637905+sylvain-schmitt@users.noreply.github.com>
2020-11-30 16:00:00 +0100

    feat: add download annonces in pdf with dompdf

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Repository\AnnoncesRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class MainController extends AbstractController
@@ -44,5 +47,50 @@ class MainController extends AbstractController
         return $this->render('main/details.html.twig', [
             'annonce' => $annonce,
         ]);
+    }
+
+    /**
+     * @Route("/download/{slug}", name="annonces_download")
+     */
+    public function download($slug, AnnoncesRepository $annoncesRepo)
+    {
+        $annonce = $annoncesRepo->findOneBy(['slug' => $slug]);
+
+        if (!$annonce) {
+            throw new NotFoundHttpException('Pas d\'annonce trouvée');
+        }
+
+               // On définit les options du PDF
+               $pdfOptions = new Options();
+               // Police par défaut
+               $pdfOptions->set('defaultFont', 'Arial');
+               $pdfOptions->setIsRemoteEnabled(true);
+       
+               // On instancie Dompdf
+               $dompdf = new Dompdf($pdfOptions);
+               $context = stream_context_create([
+                   'ssl' => [
+                       'verify_peer' => FALSE,
+                       'verify_peer_name' => FALSE,
+                       'allow_self_signed' => TRUE
+                   ]
+               ]);
+               $dompdf->setHttpContext($context);
+       
+               // On génère le html
+               $html = $this->renderView('main/download.html.twig', [
+                'annonce' => $annonce,
+            ]);
+       
+               $dompdf->loadHtml($html);
+               $dompdf->setPaper('A4', 'portrait');
+               $dompdf->render();
+        // On génère un nom de fichier
+        $fichier = 'annonce-' . $slug .'.pdf';
+
+        // On envoie le PDF au navigateur
+        $dompdf->stream($fichier, [
+            'Attachment' => true
+        ]);               return new Response();
     }
 }
